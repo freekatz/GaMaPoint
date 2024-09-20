9b9c5dc3b1d5ad44bcfd0746f1d13b8d5b5650fd
freekatz <katz@mtxai.io>
2024-09-20 23:18:21 +0800

    obj dict support deepcopy

--- utils/dict_utils.py
+++ utils/dict_utils.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class ObjDict(dict):
     """
     Makes a  dictionary behave like an object,with attribute-style access.
@@ -8,3 +11,13 @@ class ObjDict(dict):
 
     def __setattr__(self, name, value):
         self[name] = value
+
+    def __deepcopy__(self, name):
+        copy_dict = dict()
+        for key, value in self.items():
+            if hasattr(value, '__deepcopy__'):
+                copy_dict[key] = copy.deepcopy(value)
+            else:
+                copy_dict[key] = value
+        return ObjDict(copy_dict)
+
