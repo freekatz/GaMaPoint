acc0a275e43c16410790ac0340833f98debfd9ee
freekatz <katz@mtxai.io>
2024-09-23 15:06:23 +0800

    update config

--- s3dis/configs/s3dis_s.py
+++ s3dis/configs/s3dis_s.py
@@ -28,10 +28,11 @@ class GaMaConfig(BaseConfig):
     num_classes = 13
     bn_momentum = 0.02
     drop_path = 0.1
+    channel_list = [64, 192, 384, 768]
     encoder_cfg = BaseConfig()
     encoder_cfg.name = 'EncoderConfig'
     encoder_cfg.in_channels = 4
-    encoder_cfg.channel_list = [64, 192, 384, 768]
+    encoder_cfg.channel_list = channel_list
     encoder_cfg.mamba_blocks = [1, 1, 1, 1]
     encoder_cfg.res_blocks = [2, 2, 4, 2]
     encoder_cfg.mlp_ratio = 2.
@@ -43,7 +44,7 @@ class GaMaConfig(BaseConfig):
 
     decoder_cfg = BaseConfig()
     decoder_cfg.name = 'DecoderConfig'
-    decoder_cfg.channel_list = [64, 128, 256, 512]
+    decoder_cfg.channel_list = channel_list
     decoder_cfg.out_channels = 256
     decoder_cfg.bn_momentum = bn_momentum
     decoder_cfg.head_drops = torch.linspace(0., 0.15, len(encoder_cfg.res_blocks)).tolist()
