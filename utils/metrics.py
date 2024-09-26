67c4e510a2890cbc36e4d5f735adb0bc7bf3c04b
freekatz <katz@mtxai.io>
2024-09-27 03:56:16 +0800

    support visible sample

--- utils/metrics.py
+++ utils/metrics.py
@@ -30,31 +30,6 @@ class Timer:
         return desc
 
 
-def PSNR(mse, peak=1.):
-    return 10 * log10((peak ** 2) / mse)
-
-
-class SegMetric:
-    def __init__(self, values=0.):
-        assert isinstance(values, dict)
-        self.miou = values.miou
-        self.oa = values.get('oa', None) 
-        self.miou = values.miou
-        self.miou = values.miou
-
-
-    def better_than(self, other):
-        if self.acc > other.acc:
-            return True
-        else:
-            return False
-
-    def state_dict(self):
-        _dict = dict()
-        _dict['acc'] = self.acc
-        return _dict
-
-
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
