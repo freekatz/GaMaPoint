01f9fad66c3c727da5c24e25f87ab534a56d613c
freekatz <katz@mtxai.io>
2024-09-28 01:39:00 +0800

    update train

--- scannetv2/train.py
+++ scannetv2/train.py
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_workers', type=int, required=False, default=12)
 
     # for train
-    parser.add_argument('--epochs', type=int, required=False, default=100)
+    parser.add_argument('--epochs', type=int, required=False, default=200)
     parser.add_argument("--warmup_epochs", type=int, required=False, default=10)
     parser.add_argument("--lr", type=float, required=False, default=1e-3)
     parser.add_argument("--lr_decay", type=float, required=False, default=1.)
