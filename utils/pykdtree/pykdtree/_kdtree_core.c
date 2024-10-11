b3399642390834b3f6411abeb98fabff1ea8ea15
freekatz <katz@mtxai.io>
2024-10-12 02:07:21 +0800

    up kdtree

--- utils/pykdtree/pykdtree/_kdtree_core.c
+++ utils/pykdtree/pykdtree/_kdtree_core.c
@@ -521,26 +521,15 @@ float calc_dist_float(float *point1_coord, float *code1, float *point2_coord, fl
 
     // alpha != 0
     float dist2 = 0;
-    uint8_t visible1 = 0;
-    uint8_t visible2 = 0;
-    int8_t j;
-    for (j = 0; j < code_dims; j++)
-    {
-        if (code1[j] != 0 && visible1 == 0) {
-            visible1 += 1;
+    if (alpha != 0) {
+        int8_t j;
+        for (j = 0; j < code_dims; j++)
+        {
+            dist2 += code2[j] * code1[j];
         }
-        if (code2[j] != 0 && visible2 == 0) {
-            visible2 += 1;
-        }
-        dist2 += abs(code2[j] - code1[j]);
+        dist2 = 1.0 - dist2 / code_dims;
     }
     // float dist2 = 1 - cosine_similarity_double(code1, code2, code_dims);
-
-    if (visible1 == 0 || visible2 == 0) {
-        dist2 = 1.0;
-    } else {
-        dist2 = dist2 / code_dims;
-    }
 
     if (alpha < 0) {
         return dist2;
@@ -1210,26 +1199,15 @@ double calc_dist_double(double *point1_coord, double *code1, double *point2_coor
 
     // alpha != 0
     double dist2 = 0;
-    uint8_t visible1 = 0;
-    uint8_t visible2 = 0;
-    int8_t j;
-    for (j = 0; j < code_dims; j++)
-    {
-        if (code1[j] != 0 && visible1 == 0) {
-            visible1 += 1;
+    if (alpha != 0) {
+        int8_t j;
+        for (j = 0; j < code_dims; j++)
+        {
+            dist2 += code2[j] * code1[j];
         }
-        if (code2[j] != 0 && visible2 == 0) {
-            visible2 += 1;
-        }
-        dist2 += abs(code2[j] - code1[j]);
+        dist2 = 1.0 - dist2 / code_dims;
     }
     // float dist2 = 1 - cosine_similarity_double(code1, code2, code_dims);
-
-    if (visible1 == 0 || visible2 == 0) {
-        dist2 = 1.0;
-    } else {
-        dist2 = dist2 / code_dims;
-    }
 
     if (alpha < 0) {
         return dist2;
