5cee252d54849c7cf9b238cb283257f2fe51b635
freekatz <katz@mtxai.io>
2024-10-12 01:52:45 +0800

    up kdtree

--- utils/pykdtree/pykdtree/_kdtree_core.c
+++ utils/pykdtree/pykdtree/_kdtree_core.c
@@ -521,25 +521,26 @@ float calc_dist_float(float *point1_coord, float *code1, float *point2_coord, fl
 
     // alpha != 0
     float dist2 = 0;
-    uint8_t visible_count1 = 0;
-    uint8_t visible_count2 = 0;
+    uint8_t visible1 = 0;
+    uint8_t visible2 = 0;
     int8_t j;
     for (j = 0; j < code_dims; j++)
     {
-        if (code1[j] != 0) {
-            visible_count1 += 1;
+        if (code1[j] != 0 && visible1 == 0) {
+            visible1 += 1;
         }
-        if (code2[j] != 0) {
-            visible_count2 += 1;
+        if (code2[j] != 0 && visible2 == 0) {
+            visible2 += 1;
         }
         dist2 += abs(code2[j] - code1[j]);
     }
     // float dist2 = 1 - cosine_similarity_double(code1, code2, code_dims);
 
-    if (visible_count1 == 0 || visible_count2 == 0) {
-        dist2 = code_dims;
+    if (visible1 == 0 || visible2 == 0) {
+        dist2 = 1.0;
+    } else {
+        dist2 = dist2 / code_dims;
     }
-    dist2 = dist2 / code_dims;
 
     if (alpha < 0) {
         return dist2;
@@ -1209,25 +1210,26 @@ double calc_dist_double(double *point1_coord, double *code1, double *point2_coor
 
     // alpha != 0
     double dist2 = 0;
-    uint8_t visible_count1 = 0;
-    uint8_t visible_count2 = 0;
+    uint8_t visible1 = 0;
+    uint8_t visible2 = 0;
     int8_t j;
     for (j = 0; j < code_dims; j++)
     {
-        if (code1[j] != 0) {
-            visible_count1 += 1;
+        if (code1[j] != 0 && visible1 == 0) {
+            visible1 += 1;
         }
-        if (code2[j] != 0) {
-            visible_count2 += 1;
+        if (code2[j] != 0 && visible2 == 0) {
+            visible2 += 1;
         }
         dist2 += abs(code2[j] - code1[j]);
     }
     // float dist2 = 1 - cosine_similarity_double(code1, code2, code_dims);
 
-    if (visible_count1 == 0 || visible_count2 == 0) {
-        dist2 = code_dims;
+    if (visible1 == 0 || visible2 == 0) {
+        dist2 = 1.0;
+    } else {
+        dist2 = dist2 / code_dims;
     }
-    dist2 = dist2 / code_dims;
 
     if (alpha < 0) {
         return dist2;
