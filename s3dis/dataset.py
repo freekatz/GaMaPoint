219f9232374932e5b6f4109af555c41f6446fb8a
freekatz <katz@mtxai.io>
2024-10-12 16:44:23 +0800

    up

--- s3dis/dataset.py
+++ s3dis/dataset.py
@@ -40,7 +40,8 @@ class S3DIS(Dataset):
                  train=True,
                  warmup=False,
                  voxel_max=24000,
-                 k=[16, 16, 16, 16],
+                 k=[24, 24, 24, 24],
+                 k_gs=[6, 6, 6, 6],
                  grid_size=[0.08, 0.16, 0.32],
                  visible_sample_stride=0.,
                  alpha=0.,
@@ -54,6 +55,7 @@ class S3DIS(Dataset):
         self.warmup = warmup
         self.voxel_max = voxel_max
         self.k = k
+        self.k_gs = k_gs
         self.grid_size = grid_size
         self.visible_sample_stride = visible_sample_stride
         self.alpha = alpha
@@ -138,7 +140,7 @@ class S3DIS(Dataset):
         gs.gs_points.__update_attr__('f', feature)
         gs.gs_points.__update_attr__('y', lbl)
         gs.projects(xyz, cam_seed=idx, cam_batch=gs.opt.n_cameras*2)
-        gs.gs_points = make_gs_points(gs.gs_points, self.k, self.grid_size, None,
+        gs.gs_points = make_gs_points(gs.gs_points, self.k, self.k_gs, self.grid_size, None,
                                       up_sample=True, visible_sample_stride=self.visible_sample_stride)
         return gs
 
@@ -162,7 +164,7 @@ class S3DIS(Dataset):
         gs.gs_points.__update_attr__('f', feature)
         gs.gs_points.__update_attr__('y', lbl)
         gs.projects(xyz, cam_seed=idx, cam_batch=gs.opt.n_cameras*2)
-        gs.gs_points = make_gs_points(gs.gs_points, self.k, self.grid_size, None,
+        gs.gs_points = make_gs_points(gs.gs_points, self.k, self.k_gs, self.grid_size, None,
                                       up_sample=True, visible_sample_stride=self.visible_sample_stride,
                                       alpha=self.alpha)
         return gs
