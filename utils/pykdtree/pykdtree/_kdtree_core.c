e1e404bf6ecf946c31758dde6f83bb48ee63097f
freekatz <katz@mtxai.io>
2024-10-12 17:24:14 +0800

    up

--- utils/pykdtree/pykdtree/_kdtree_core.c
+++ utils/pykdtree/pykdtree/_kdtree_core.c
@@ -515,9 +515,9 @@ float calc_dist_float(float *point1_coord, uint8_t *code1, float *point2_coord, 
     int8_t j;
     for (j = 0; j < code_dims; j++)
     {
-        dist2 = code2[i] * code1[i];
+        dist2 = code2[j] & code1[j];
     }
-    dist2 = dist2 / code_dims;
+    dist2 = 1.0 - dist2 / code_dims;
     if (alpha < 0) {
         return dist2;
     }
@@ -526,7 +526,6 @@ float calc_dist_float(float *point1_coord, uint8_t *code1, float *point2_coord, 
     dist2 = dist2 * scaler * alpha;
     return dist1 + dist2;
 }
-
 
 /************************************************
 Get squared distance from point to cube in specified dimension
@@ -1189,9 +1188,9 @@ double calc_dist_double(double *point1_coord, uint8_t *code1, double *point2_coo
     int8_t j;
     for (j = 0; j < code_dims; j++)
     {
-        dist2 = code2[i] * code1[i];
+        dist2 = code2[j] & code1[j];
     }
-    dist2 = dist2 / code_dims;
+    dist2 = 1.0 - dist2 / code_dims;
     if (alpha < 0) {
         return dist2;
     }
