6645c777082f7de36216dde5dee440d0c45196b9
freekatz <katz@mtxai.io>
2024-10-13 22:11:25 +0800

    add model && scanobj l size

--- modelnet40/configs/__init__.py
+++ modelnet40/configs/__init__.py
@@ -3,8 +3,9 @@ import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
 
-from modelnet40.configs import modelnet40_s
+from modelnet40.configs import modelnet40_s, modelnet40_l
 
 model_configs = {
     's': modelnet40_s.ModelConfig(),
+    'l': modelnet40_l.ModelConfig(),
 }
---
+++ modelnet40/configs/modelnet40_l.py
@@ -0,0 +1,52 @@
+import __init__
+
+import torch
+
+from backbone.gs_3d import GaussianOptions
+from backbone.mamba_ssm.models import MambaConfig
+from utils.config import EasyConfig
+
+
+class ModelNet40Config(EasyConfig):
+    def __init__(self):
+        super().__init__()
+        self.name = 'ModelNet40Config'
+        self.k = [20, 20, 20, 20]
+        self.use_gs = False
+        self.k_gs = [5, 5, 5, 5]
+        self.n_samples = [1024, 512, 256, 64]
+        self.visible_sample_stride = 0.
+        self.num_points = 1024
+        gs_opts = GaussianOptions.default()
+        gs_opts.n_cameras = 8
+        gs_opts.cam_fovy = 120
+        self.gs_opts = gs_opts
+        self.alpha = 0.25
+
+
+class ModelConfig(EasyConfig):
+    def __init__(self):
+        super().__init__()
+        self.name = 'ModelConfig'
+        self.train_cfg = ModelNet40Config()
+        self.num_classes = 40
+        self.bn_momentum = 0.1
+        drop_path = 0.15
+        stage_cfg = EasyConfig()
+        stage_cfg.name = 'StageConfig'
+        stage_cfg.in_channels = 1
+        stage_cfg.channel_list = [64, 128, 256, 512]
+        stage_cfg.head_channels = 2048
+        stage_cfg.mamba_blocks = [1, 1, 1, 1]
+        stage_cfg.res_blocks = [4, 4, 8, 4]
+        stage_cfg.mlp_ratio = 2.
+        stage_cfg.beta = self.train_cfg.alpha
+        stage_cfg.use_gs = self.train_cfg.use_gs
+        stage_cfg.bn_momentum = self.bn_momentum
+        drop_rates = torch.linspace(0., drop_path, sum(stage_cfg.res_blocks)).split(stage_cfg.res_blocks)
+        stage_cfg.drop_paths = [d.tolist() for d in drop_rates]
+        stage_cfg.mamba_cfg = MambaConfig.default()
+        stage_cfg.hybrid_args = {'hybrid': False}  # whether hybrid mha, {'hybrid': True, 'type': 'post', 'ratio': 0.5}
+        stage_cfg.diff_factor = 40.
+        stage_cfg.diff_std = [1.6, 3.0, 5.8, 11.4]
+        self.stage_cfg = stage_cfg
