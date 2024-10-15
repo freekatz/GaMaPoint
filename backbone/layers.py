fe3459b790ccda0e4d030491e99b626c19daa143
freekatz <katz@mtxai.io>
2024-10-16 01:56:02 +0800

    add pos embed

--- backbone/layers.py
+++ backbone/layers.py
@@ -266,7 +266,7 @@ class PointMambaLayer(nn.Module):
         self.layer_index = layer_index
         self.config = config
 
-        self.pos_embed = nn.Parameter(torch.randn([1, gs_opts.n_cameras * 2, channels], dtype=torch.float32))
+        self.pos_embed = nn.Parameter(torch.randn([gs_opts.n_cameras * 2, channels], dtype=torch.float32))
         self.mlp = Mlp(
             in_channels=gs_opts.n_cameras * 2,
             hidden_channels=gs_opts.n_cameras * 2 * 2,
@@ -278,10 +278,11 @@ class PointMambaLayer(nn.Module):
 
     def forward(self, f, f_gs, gs: NaiveGaussian3D):
         assert len(f.shape) == 2
-        f = f.unsqueeze(0)
-        visible = f_gs[:, 3:4, :].squeeze(1)
-        pos_embed = self.mlp(visible.unsqueeze(0)) @ self.pos_embed
+        visible = f_gs[:, 2:3, :].squeeze(1)
+        f_visible = self.mlp(visible.unsqueeze(0)).squeeze(0)
+        pos_embed = f_visible @ self.pos_embed
         f = f + pos_embed
+        f = f.unsqueeze(0)
         B, N, C = f.shape
         f = f + self.mixer(input_ids=f, mask=None, gs=gs, order=None)
         f = self.bn(f.view(B * N, -1)).view(B, N, -1)
