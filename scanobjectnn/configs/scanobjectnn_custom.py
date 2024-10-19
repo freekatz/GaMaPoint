1a917f6a311be636a1ba232d55b97e03f318bc30
freekatz <katz@mtxai.io>
2024-10-19 09:13:03 +0800

    up obj

--- scanobjectnn/configs/scanobjectnn_custom.py
+++ scanobjectnn/configs/scanobjectnn_custom.py
@@ -12,9 +12,9 @@ class ScanObjectNNConfig(EasyConfig):
         super().__init__()
         self.name = 'ScanObjectNNConfig'
         self.k = [32, 32, 32]
-        self.n_samples = [2048, 512, 128]
+        self.n_samples = [1024, 256, 64]
         self.visible_sample_stride = 0.
-        self.num_points = 2048
+        self.num_points = 1024
         gs_opts = GaussianOptions.default()
         gs_opts.n_cameras = 8
         gs_opts.cam_fovy = 120
@@ -33,7 +33,7 @@ class ModelConfig(EasyConfig):
         backbone_cfg = EasyConfig()
         backbone_cfg.name = 'BackboneConfig'
         backbone_cfg.in_channels = 4
-        backbone_cfg.channel_list = [96, 192, 384]
+        backbone_cfg.channel_list = [128, 384, 768]
         backbone_cfg.head_channels = 2048
         backbone_cfg.mamba_blocks = [1, 1, 1]
         backbone_cfg.res_blocks = [4, 4, 4]
