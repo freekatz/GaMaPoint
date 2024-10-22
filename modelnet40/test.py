6a3faca14acb3f5ab682a26a354a554c99ac8ac1
freekatz <katz@mtxai.io>
2024-10-23 02:07:17 +0800

    cal flops

--- modelnet40/test.py
+++ modelnet40/test.py
@@ -49,7 +49,7 @@ def cal_flops(cfg, model):
         batch_size=1,
         gs_opts=cfg.model_cfg.train_cfg.gs_opts
     )
-    gs = merge_gs_list([ds[0]])
+    gs = merge_gs_list([ds[0]], up_sample=False)
     gs.gs_points.to_cuda(non_blocking=True)
     cal_model_flops(model, inputs=dict(gs=gs))
 
